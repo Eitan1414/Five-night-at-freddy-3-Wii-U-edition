9219d82897ef17bcbf76b7e06c419559ec92a620
Pixel Electronics Studio <affairegamer@gmail.com>
2026-08-04 22:37:37 +0200

    assets: declare camera 04-10 and Springtrap textures

---
+++ include/assets/camera_extended_assets.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "renderer/texture.h"
+
+extern const TextureRle gCamera04Texture;
+extern const TextureRle gCamera05Texture;
+extern const TextureRle gCamera06Texture;
+extern const TextureRle gCamera07Texture;
+extern const TextureRle gCamera08Texture;
+extern const TextureRle gCamera09Texture;
+extern const TextureRle gCamera10Texture;
+extern const TextureRle gSpringtrap01Sprite;
+extern const TextureRle gSpringtrap02Sprite;
+extern const TextureRle gSpringtrap03Sprite;
+extern const TextureRle gSpringtrap04Sprite;
+extern const TextureRle gSpringtrap05Sprite;
+extern const TextureRle gSpringtrap06Sprite;
+extern const TextureRle gSpringtrap07Sprite;
+extern const TextureRle gSpringtrap08Sprite;
+extern const TextureRle gSpringtrap09Sprite;
+extern const TextureRle gSpringtrap10Sprite;
