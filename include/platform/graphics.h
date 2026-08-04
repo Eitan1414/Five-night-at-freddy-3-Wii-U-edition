0fcf05aa7b017a7deba7da022aec92f5f49c61c0
Pixel Electronics Studio <affairegamer@gmail.com>
2026-08-04 15:09:51 +0200

    refactor: add portable graphics interface

---
+++ include/platform/graphics.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#define GRAPHICS_LOGICAL_WIDTH 854
+#define GRAPHICS_LOGICAL_HEIGHT 480
+
+#define GRAPHICS_RGB(r, g, b) \
+    ((((uint32_t) (r)) << 24) | (((uint32_t) (g)) << 16) | \
+     (((uint32_t) (b)) << 8) | 0xFFu)
+
+typedef enum GraphicsTarget {
+    GRAPHICS_TARGET_TV = 1u << 0,
+    GRAPHICS_TARGET_GAMEPAD = 1u << 1,
+    GRAPHICS_TARGET_BOTH = GRAPHICS_TARGET_TV | GRAPHICS_TARGET_GAMEPAD
+} GraphicsTarget;
+
+bool graphics_init(void);
+void graphics_shutdown(void);
+
+void graphics_clear(uint32_t targets, uint32_t colour);
+void graphics_present(uint32_t targets);
+
+void graphics_draw_rect(uint32_t targets,
+                        int x,
+                        int y,
+                        int width,
+                        int height,
+                        uint32_t colour);
+void graphics_draw_frame(uint32_t targets,
+                         int x,
+                         int y,
+                         int width,
+                         int height,
+                         int thickness,
+                         uint32_t colour);
+void graphics_draw_ellipse(uint32_t targets,
+                           int centre_x,
+                           int centre_y,
+                           int radius_x,
+                           int radius_y,
+                           uint32_t colour);
+void graphics_draw_text(uint32_t targets,
+                        int x,
+                        int y,
+                        int scale,
+                        const char *text,
+                        uint32_t colour);
