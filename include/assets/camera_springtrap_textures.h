179815cb6644963ac7cd50f839841319d77abf1d
Pixel Electronics Studio <affairegamer@gmail.com>
2026-08-04 18:54:27 +0200

    feat: add Springtrap camera texture declarations

---
+++ include/assets/camera_springtrap_textures.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "renderer/texture.h"
+
+extern const TextureRle gCamera01SpringtrapTexture;
+extern const TextureRle gCamera02SpringtrapTexture;
+extern const TextureRle gCamera03SpringtrapTexture;
