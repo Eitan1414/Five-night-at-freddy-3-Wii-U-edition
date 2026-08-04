1b555f9231219655a5e2f5017eeea4a87dbfa443
Pixel Electronics Studio <affairegamer@gmail.com>
2026-08-05 00:08:14 +0200

    assets: declare Phantom PSX textures

---
+++ include/assets/phantom_assets.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "renderer/texture.h"
+
+extern const TextureRle gPhantomFoxyOfficeTexture;
+extern const TextureRle gPhantomFreddyOfficeTexture;
+extern const TextureRle gPhantomChicaOfficeTexture;
+extern const TextureRle gPhantomMangleOfficeTexture;
+extern const TextureRle gPhantomPuppetOfficeTexture;
+extern const TextureRle gPhantomBBCameraTexture;
+extern const TextureRle gPhantomChicaCameraTexture;
+extern const TextureRle gPhantomMangleCameraTexture;
+extern const TextureRle gPhantomPuppetCameraTexture;
+extern const TextureRle gPhantomBBJumpscare1;
+extern const TextureRle gPhantomFoxyJumpscare1;
+extern const TextureRle gPhantomFreddyJumpscare1;
+extern const TextureRle gPhantomChicaJumpscare1;
+extern const TextureRle gPhantomPuppetJumpscare1;
+extern const TextureRle gPhantomBBJumpscare2;
+extern const TextureRle gPhantomFoxyJumpscare2;
+extern const TextureRle gPhantomFreddyJumpscare2;
+extern const TextureRle gPhantomChicaJumpscare2;
+extern const TextureRle gPhantomPuppetJumpscare2;
+extern const TextureRle gPhantomBBJumpscare3;
+extern const TextureRle gPhantomFoxyJumpscare3;
+extern const TextureRle gPhantomFreddyJumpscare3;
+extern const TextureRle gPhantomChicaJumpscare3;
+extern const TextureRle gPhantomPuppetJumpscare3;
+extern const TextureRle gPhantomBBJumpscare4;
+extern const TextureRle gPhantomFoxyJumpscare4;
+extern const TextureRle gPhantomFreddyJumpscare4;
+extern const TextureRle gPhantomChicaJumpscare4;
+extern const TextureRle gPhantomPuppetJumpscare4;
+extern const TextureRle gPhantomBBJumpscare5;
+extern const TextureRle gPhantomFoxyJumpscare5;
+extern const TextureRle gPhantomFreddyJumpscare5;
+extern const TextureRle gPhantomChicaJumpscare5;
+extern const TextureRle gPhantomPuppetJumpscare5;
+extern const TextureRle *const gPhantomBBJumpscareFrames[5];
+extern const TextureRle *const gPhantomFoxyJumpscareFrames[5];
+extern const TextureRle *const gPhantomFreddyJumpscareFrames[5];
+extern const TextureRle *const gPhantomChicaJumpscareFrames[5];
+extern const TextureRle *const gPhantomPuppetJumpscareFrames[5];
