9a1aa846b3db2112839fc491a130777ccd7e1a09
Pixel Electronics Studio <affairegamer@gmail.com>
2026-08-05 17:39:16 +0200

    Expose secret minigame progress APIs

--- include/game/progress_save.h
+++ include/game/progress_save.h
@@ -1,16 +1,21 @@
 #pragma once
 
 #include <stdbool.h>
+#include <stdint.h>
 
 void progress_save_init(int *unlocked_night);
 void progress_save_shutdown(void);
 
 void progress_save_complete_night(int completed_night,
                                   int *unlocked_night);
+void progress_save_complete_secret_minigame(int minigame);
 
 int progress_save_highest_unlocked_night(void);
 bool progress_save_is_night_completed(int night);
 bool progress_save_extras_unlocked(void);
+uint8_t progress_save_secret_minigames_mask(void);
+bool progress_save_is_secret_minigame_completed(int minigame);
+bool progress_save_good_ending_unlocked(void);
 
 const char *progress_save_load_status_text(void);
 bool progress_save_load_status_is_error(void);
