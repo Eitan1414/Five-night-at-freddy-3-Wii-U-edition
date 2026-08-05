1fa0bf8900f479353db6a4e17c1cac4505f8f8c2
Pixel Electronics Studio <affairegamer@gmail.com>
2026-08-05 19:14:41 +0200

    Expose Aggressive Nightmare completion progress

--- include/game/progress_save.h
+++ include/game/progress_save.h
@@ -9,6 +9,7 @@ void progress_save_shutdown(void);
 void progress_save_complete_night(int completed_night,
                                   int *unlocked_night);
 void progress_save_complete_secret_minigame(int minigame);
+void progress_save_complete_aggressive_nightmare(void);
 
 int progress_save_highest_unlocked_night(void);
 bool progress_save_is_night_completed(int night);
@@ -16,6 +17,7 @@ bool progress_save_extras_unlocked(void);
 uint8_t progress_save_secret_minigames_mask(void);
 bool progress_save_is_secret_minigame_completed(int minigame);
 bool progress_save_good_ending_unlocked(void);
+bool progress_save_aggressive_nightmare_completed(void);
 
 const char *progress_save_load_status_text(void);
 bool progress_save_load_status_is_error(void);
