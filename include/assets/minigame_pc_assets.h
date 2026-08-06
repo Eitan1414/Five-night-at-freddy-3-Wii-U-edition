6601cb9074b4445f8ec6087f888b733d3664955d
Pixel Electronics Studio <affairegamer@gmail.com>
2026-08-06 15:36:34 +0200

    Add PC minigame asset declarations

---
+++ include/assets/minigame_pc_assets.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "renderer/texture.h"
+
+extern const TextureRle gMiniBbLeft;
+extern const TextureRle gMiniBbRight;
+extern const TextureRle gMiniBalloonSmallRed;
+extern const TextureRle gMiniBalloonRed;
+extern const TextureRle gMiniBalloonPurple;
+extern const TextureRle gMiniBalloonGreen;
+extern const TextureRle gMiniBalloonOrange;
+extern const TextureRle gMiniBalloonLargeRed;
+extern const TextureRle gMiniExitCloud;
