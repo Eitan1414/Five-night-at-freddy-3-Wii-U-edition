c5ee5ca85003c9824ac4da2415a398a567468742
Pixel Electronics Studio <affairegamer@gmail.com>
2026-08-06 19:59:53 +0200

    Add original PC minigame asset declarations

---
+++ include/assets/minigame_pc_extended_assets.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "renderer/texture.h"
+
+extern const TextureRle gMiniMangleLeft;
+extern const TextureRle gMiniMangleRight;
+extern const TextureRle gMiniManglePart1;
+extern const TextureRle gMiniManglePart2;
+extern const TextureRle gMiniManglePart3;
+extern const TextureRle gMiniManglePart4;
+extern const TextureRle gMiniChildSad;
+extern const TextureRle gMiniChildHappy;
+extern const TextureRle gMiniChicaLeft;
+extern const TextureRle gMiniChicaRight;
+extern const TextureRle gMiniCupcakeSmall;
+extern const TextureRle gMiniCupcakeLarge;
+extern const TextureRle gMiniStageRight;
+extern const TextureRle gMiniStageLeft;
+extern const TextureRle gMiniStageFront;
+extern const TextureRle gMiniStageChild;
+extern const TextureRle gMiniShadowLeft;
+extern const TextureRle gMiniShadowRight;
+extern const TextureRle gMiniShadowChildSad;
+extern const TextureRle gMiniShadowChildHappy;
+extern const TextureRle gMiniPuppetLeft;
+extern const TextureRle gMiniPuppetRight;
+extern const TextureRle gMiniPartyChild1;
+extern const TextureRle gMiniPartyChild2;
+extern const TextureRle gMiniPartyChild3;
+extern const TextureRle gMiniPartyChild4;
+extern const TextureRle gMiniPartyChild5;
+extern const TextureRle gMiniPartyCake;
+extern const TextureRle gMiniExitDoor;
