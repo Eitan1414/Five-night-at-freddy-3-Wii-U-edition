839e12d054c7757524492325d1853081886dd5c1
Pixel Electronics Studio <affairegamer@gmail.com>
2026-08-09 13:26:14 +0200

    Wire supplied Phantom Mangle sprite override

---
+++ include/assets/phantom_mangle_override.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "assets/phantom_mangle_user_texture.h"
+
+/* Use the supplied transparent Phantom Mangle sprite everywhere Mangle is rendered. */
+#define gPhantomMangleCameraTexture gPhantomMangleUserTexture
+#define gPhantomMangleOfficeTexture gPhantomMangleUserTexture
