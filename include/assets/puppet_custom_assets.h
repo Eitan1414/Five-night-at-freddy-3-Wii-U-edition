a4f350b46a2ef142a8d2463bde2eb214264b18f0
Pixel Electronics Studio <affairegamer@gmail.com>
2026-08-09 04:20:57 +0200

    Remove redundant custom Puppet header

--- include/assets/puppet_custom_assets.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "assets/jumpscare_assets.h"
-
-/* Five-frame Phantom Puppet/Marionette animation from the supplied sprite sheet. */
-extern const JumpscareSequence gPhantomPuppetSheetAnimation;
