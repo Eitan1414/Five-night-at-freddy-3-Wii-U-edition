74b6411b21ab128da6384c4d8f292f1db5424f63
Pixel Electronics Studio <affairegamer@gmail.com>
2026-08-14 19:18:52 +0200

    Add Wii U language and subtitle settings

--- include/game/wiiu_controls.h
+++ include/game/wiiu_controls.h
@@ -9,7 +9,20 @@ typedef enum WiiUControlMode {
     WIIU_CONTROL_MODE_COUNT
 } WiiUControlMode;
 
+typedef enum WiiULanguage {
+    WIIU_LANGUAGE_ENGLISH = 0,
+    WIIU_LANGUAGE_FRENCH = 1,
+    WIIU_LANGUAGE_COUNT
+} WiiULanguage;
+
 void wiiu_controls_init(void);
 WiiUControlMode wiiu_controls_mode(void);
 void wiiu_controls_set_mode(WiiUControlMode mode);
 const char *wiiu_controls_mode_name(WiiUControlMode mode);
+
+WiiULanguage wiiu_controls_language(void);
+void wiiu_controls_set_language(WiiULanguage language);
+const char *wiiu_controls_language_name(WiiULanguage language);
+
+bool wiiu_controls_subtitles_enabled(void);
+void wiiu_controls_set_subtitles_enabled(bool enabled);
