0dbf72f5965beb7be673ba95a0f69d6437be4f7a
Pixel Electronics Studio <affairegamer@gmail.com>
2026-08-15 02:01:38 +0200

    Add exact PC monitor fidelity asset declarations

---
+++ include/assets/monitor_fidelity_assets.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "renderer/texture.h"
+
+extern const TextureRle gPcMonitorNodeSelectedTexture;
+extern const TextureRle gPcMonitorCam01LabelTexture;
+extern const TextureRle gPcMonitorCam02LabelTexture;
+extern const TextureRle gPcMonitorCam03LabelTexture;
+extern const TextureRle gPcMonitorCam04LabelTexture;
+extern const TextureRle gPcMonitorCam05LabelTexture;
+extern const TextureRle gPcMonitorCam06LabelTexture;
+extern const TextureRle gPcMonitorCam07LabelTexture;
+extern const TextureRle gPcMonitorCam08LabelTexture;
+extern const TextureRle gPcMonitorCam09LabelTexture;
+extern const TextureRle gPcMonitorCam10LabelTexture;
+extern const TextureRle gPcMaintCursorTexture;
+extern const TextureRle gPcMaintExitTexture;
+extern const TextureRle gPcMaintProgress0Texture;
+extern const TextureRle gPcMaintProgress1Texture;
+extern const TextureRle gPcMaintProgress2Texture;
+extern const TextureRle gPcMaintProgress3Texture;
+extern const TextureRle gPcMaintProgress4Texture;
+
+extern const TextureRle *const gPcMonitorCameraLabelTextures[10];
+extern const TextureRle *const gPcMaintProgressTextures[5];
